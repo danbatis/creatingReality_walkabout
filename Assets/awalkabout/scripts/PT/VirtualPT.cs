f75cee93515682ab81ca551d71542594e6d8bfcb
Jivitesh Dhaliwal <jiviteshdhaliwal@gmail.com>
2018-03-14 10:31:34 -0700

    Added the mountainfeedback prefab and updated script

--- Assets/awalkabout/scripts/PT/VirtualPT.cs
+++ Assets/awalkabout/scripts/PT/VirtualPT.cs
@@ -10,9 +10,15 @@ public class VirtualPT : MonoBehaviour {
     public AudioClip badGlobSound;
     public AudioClip mushroomSquishSound;
 
+    public GameObject mountainFeedback;
+
     public int goodGlobsCollected;
     public int badGlobsCollected;
     public int obstaclesCrushed;
+    public int feedbackLayer = 0;
+    public int maxFeedbackLayer = 7;
+
+    public int feedbackAfterNumberOfGlobsConnected;
 
     int rightControllerGlobs;
     int leftControllerGlobs;
@@ -31,6 +37,14 @@ public class VirtualPT : MonoBehaviour {
         } else {
             leftControllerGlobs += 1;
         }
+        if (goodGlobsCollected % feedbackAfterNumberOfGlobsConnected == 0) {
+            // then show the feedback on the mountains
+            if (feedbackLayer < maxFeedbackLayer) {
+                feedbackLayer += 1;
+                mountainFeedback.GetComponent<FlowerBlossoms>().GrowFlowers(feedbackLayer);
+            }
+        }
+
     }
 
 	public void BadGlobCollected(string controller) {
