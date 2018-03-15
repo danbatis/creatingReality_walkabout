8aa5294594055bba0c6d52f393af8e27c500cd95
danbatis <danielbatista.contato@gmail.com>
2018-03-14 17:45:51 -0700

    updated gui to load scenes

---
+++ Assets/awalkabout/scripts/loadScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadScenes : MonoBehaviour {
+	Dropdown sceneOption;
+
+	// Use this for initialization
+	void Start () {
+		sceneOption = GameObject.Find ("GUI/Panel/Dropdown").GetComponent<Dropdown>();		
+	}
+	
+	public void LoadScene(){
+		Debug.Log (sceneOption.options[sceneOption.value].text);
+		switch(sceneOption.options[sceneOption.value].text){
+		case "scene1":
+			SceneManager.LoadScene(0);
+			break;
+		case "scene2":
+			SceneManager.LoadScene(1);
+			break;
+		case "scene4":
+			SceneManager.LoadScene(2);
+			break;	
+		}		
+	}
+}
