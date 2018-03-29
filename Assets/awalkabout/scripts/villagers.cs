65d717b89538aec8ef5972aad62b54cbfcf732c4
danbatis <danielbatista.contato@gmail.com>
2018-03-29 00:28:53 -0700

    adding some buildings and villagers to the first level, and fixing stuff from judging day

---
+++ Assets/awalkabout/scripts/villagers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class villagers : MonoBehaviour {
+	NavMeshAgent navAgent;
+	Transform myTransform;
+	public List<Transform> destinies;
+	public int currentDest;
+	public float nextCheckpointDist = 2.0f;
+
+	public float maxPlayerDist = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent>();	
+		navAgent.SetDestination(destinies[currentDest].position);
+		navAgent.isStopped = false;
+		myTransform = transform;
+	}
+
+	// Update is called once per frame
+	void Update(){
+		if(Vector3.ProjectOnPlane(destinies[currentDest].position - myTransform.position, Vector3.up).magnitude < nextCheckpointDist){
+			currentDest++;
+			if (currentDest >= destinies.Count)
+				currentDest = 0;
+			navAgent.SetDestination(destinies[currentDest].position);
+		}
+	}
+}
